3eeac604f0802556356deb4011e41af757dd4503
saheed <saheedjimoh338@gmail.com>
2025-01-09 06:58:50 +0100

    Initial commit

--- src/main/java/com/eccomerce/Ikhlas_eccomerce/model/Category.java
+++ src/main/java/com/eccomerce/Ikhlas_eccomerce/model/Category.java
@@ -1,6 +1,7 @@
 package com.eccomerce.Ikhlas_eccomerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,8 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private  Long categoryId;
+
+    @NotBlank
     private  String categoryName;
 
 }
